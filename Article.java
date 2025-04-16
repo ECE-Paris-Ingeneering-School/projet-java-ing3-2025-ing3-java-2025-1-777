08372e93f1d5fa4acb96b222b6ed2a049b79ef01
MALAK0010 <malak.mountassibelidrissi@edu.ece.fr>
2025-04-16 12:42:47 +0200

    Update Article.java

--- Article.java
+++ Article.java
@@ -1,65 +1,96 @@
 package model;
 
+/**
+ * Classe représentant un article à vendre.
+ */
 public class Article {
-    private int id_article;
+    private int idArticle;
     private String nom;
     private String description;
     private double prixUnitaire;
     private double prixBulk;
     private int quantiteBulk;
     private int stock;
-    private String imagePath; // Chemin vers l'image du produit
+    private String imagePath;
 
     public Article() {
+
     }
 
-  
-    public int getId_article() {
-        return id_article;
-    }
-    public void setId_article(int id_article) {
-        this.id_article = id_article;
-    }
-    public String getNom() {
-        return nom;
-    }
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    public double getPrixUnitaire() {
-        return prixUnitaire;
-    }
-    public void setPrixUnitaire(double prixUnitaire) {
-        this.prixUnitaire = prixUnitaire;
-    }
-    public double getPrixBulk() {
-        return prixBulk;
-    }
-    public void setPrixBulk(double prixBulk) {
-        this.prixBulk = prixBulk;
-    }
-    public int getQuantiteBulk() {
-        return quantiteBulk;
-    }
-    public void setQuantiteBulk(int quantiteBulk) {
-        this.quantiteBulk = quantiteBulk;
-    }
-    public int getStock() {
-        return stock;
-    }
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
+
     public String getImagePath() {
         return imagePath;
     }
+
+
+    public Article(int idArticle, String nom, String description, double prixUnitaire, double prixBulk, int quantiteBulk, int stock) {
+        this.idArticle = idArticle;
+        this.nom = nom;
+        this.description = description;
+        this.prixUnitaire = prixUnitaire;
+        this.prixBulk = prixBulk;
+        this.quantiteBulk = quantiteBulk;
+        this.stock = stock;
+
+    }
     public void setImagePath(String imagePath) {
         this.imagePath = imagePath;
+    }
+
+    // Getters et setters
+    public int getIdArticle() {
+        return idArticle;
+    }
+
+    public void setIdArticle(int idArticle) {
+        this.idArticle = idArticle;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrixUnitaire() {
+        return prixUnitaire;
+    }
+
+    public void setPrixUnitaire(double prixUnitaire) {
+        this.prixUnitaire = prixUnitaire;
+    }
+
+    public double getPrixBulk() {
+        return prixBulk;
+    }
+
+    public void setPrixBulk(double prixBulk) {
+        this.prixBulk = prixBulk;
+    }
+
+    public int getQuantiteBulk() {
+        return quantiteBulk;
+    }
+
+    public void setQuantiteBulk(int quantiteBulk) {
+        this.quantiteBulk = quantiteBulk;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
     }
 }
