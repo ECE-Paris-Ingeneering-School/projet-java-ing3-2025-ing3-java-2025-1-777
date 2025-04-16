1f5bb5a6c0ba993328fb4699b3f8f18068944513
MALAK0010 <malak.mountassibelidrissi@edu.ece.fr>
2025-04-16 21:05:11 +0200

    Create LigneCommande.java

---
+++ LigneCommande.java
@@ -0,0 +1,63 @@
+package model;
+
+/**
+ * Classe représentant les détails d’achat d’une commande .
+ */
+public class LigneCommande {
+    private int idLigne;
+    private int idCommande;
+    private int idArticle;
+    private int quantite;
+    private double prixTotal;
+
+    public LigneCommande() {
+    }
+
+    public LigneCommande(int idLigne, int idCommande, int idArticle, int quantite, double prixTotal) {
+        this.idLigne = idLigne;
+        this.idCommande = idCommande;
+        this.idArticle = idArticle;
+        this.quantite = quantite;
+        this.prixTotal = prixTotal;
+    }
+
+    public int getIdLigne() {
+        return idLigne;
+    }
+
+    public void setIdLigne(int idLigne) {
+        this.idLigne = idLigne;
+    }
+
+    public int getIdCommande() {
+        return idCommande;
+    }
+
+    public void setIdCommande(int idCommande) {
+        this.idCommande = idCommande;
+    }
+
+    public int getIdArticle() {
+        return idArticle;
+    }
+
+    public void setIdArticle(int idArticle) {
+        this.idArticle = idArticle;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
+
+    public double getPrixTotal() {
+        return prixTotal;
+    }
+
+    public void setPrixTotal(double prixTotal) {
+        this.prixTotal = prixTotal;
+    }
+}
