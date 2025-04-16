11c8e55ff4c9f6ac918542105105776a35009119
MALAK0010 <malak.mountassibelidrissi@edu.ece.fr>
2025-04-16 21:05:41 +0200

    Create Marque.java

---
+++ Marque.java
@@ -0,0 +1,33 @@
+package model;
+
+/**
+ * Classe représentant une marque d'article.
+ */
+public class Marque {
+    private int idMarque;
+    private String nom;
+
+    public Marque() {
+    }
+
+    public Marque(int idMarque, String nom) {
+        this.idMarque = idMarque;
+        this.nom = nom;
+    }
+
+    public int getIdMarque() {
+        return idMarque;
+    }
+
+    public void setIdMarque(int idMarque) {
+        this.idMarque = idMarque;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+}
