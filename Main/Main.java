458616c83b606b96ed093fd8cc413c9970c94038
MALAK0010 <malak.mountassibelidrissi@edu.ece.fr>
2025-04-23 12:57:33 +0200

    Update Main.java

--- Main/Main.java
+++ Main/Main.java
@@ -1,15 +1,30 @@
-package Main;
+package Controlers;
 
-import Controlers.ShoppingController;
-import view.CatalogFrame;
-import javax.swing.*;
 import view.LoginFrame;
+import javax.swing.SwingUtilities;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+/**
+ * Classe principale pour lancer l'application.
+ */
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            ShoppingController shoppingController = new ShoppingController();
-            new LoginFrame(shoppingController).setVisible(true);
+            new LoginFrame().setVisible(true);
         });
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:8889/Shopping?useSSL=false&serverTimezone=UTC",
+                    "root",
+                    "root"
+            );
+            System.out.println("Connexion réussie !");
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
