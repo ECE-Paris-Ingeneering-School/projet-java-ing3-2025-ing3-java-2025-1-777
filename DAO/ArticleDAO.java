9c6c0ada0ddae3d50ceb3aa23b5c9bb10af5078b
MALAK0010 <malak.mountassibelidrissi@edu.ece.fr>
2025-04-23 11:49:20 +0200

    Update ArticleDAO.java

--- DAO/ArticleDAO.java
+++ DAO/ArticleDAO.java
@@ -3,12 +3,13 @@ import model.Article;
 
 import java.util.List;
 
-/**
- * Interface DAO pour l’entité Article.
- */
+
 public interface ArticleDAO extends GenericDAO<Article> {
-    // Ajoutez d'autres méthodes spécifiques, par exemple une recherche par marque
+
     List<Article> findByMarque(int idMarque);
 
+    List<Article> findByCategory(String category);
+
     List<Article> getAllArticles();
+
 }
