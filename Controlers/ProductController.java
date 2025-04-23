eb9d19bf01e89afe27b36bfbf13d1e3e155141eb
MALAK0010 <malak.mountassibelidrissi@edu.ece.fr>
2025-04-23 11:46:21 +0200

    Update ProductController.java

--- Controlers/ProductController.java
+++ Controlers/ProductController.java
@@ -1,24 +1,47 @@
 package Controlers;
 
-
 import DAO.ArticleDAO;
 import DAO.ArticleDAOImpl;
 import model.Article;
+
 import java.util.List;
 
+
 public class ProductController {
-    private ArticleDAO articleDAO;
+
+    private final ArticleDAO articleDAO;
 
     public ProductController() {
-        articleDAO = new ArticleDAOImpl();
+        this.articleDAO = new ArticleDAOImpl();
     }
 
-    public List<Article> getCatalogue() {
+
+    public List<Article> getCatalogue(){
         return articleDAO.findAll();
     }
-
     public Article getProductById(int id) {
         return articleDAO.findById(id);
     }
+    public boolean createArticle(Article a) {
+        return articleDAO.insert(a);
+    }
+    public boolean updateArticle(Article a) {
+        return articleDAO.update(a);
+    }
+    public boolean deleteArticle(int id) {
+        return articleDAO.delete(id);
+    }
+
+
+    public boolean addProduct(Article a) {
+        return articleDAO.insert(a);
+    }
+    public boolean updateProduct(Article a){
+        return articleDAO.update(a);
+    }
+    public boolean deleteProduct(int id){
+        return articleDAO.delete(id);
+    }
 }
+
 
